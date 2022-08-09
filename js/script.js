1ee376da6a621dbd8e9efeb0405dab004720f558
Kevin Mossate <kevinmossate@gmail.com>
2022-08-09 20:01:28 -0300

    finished the basic project

--- js/script.js
+++ js/script.js
@@ -9,10 +9,24 @@ const jump = () => {
   }, 500);
 };
 const loopGame = setInterval(() => {
-  pipe.style.animation = "none";
-  pipe.style.left = `${pipePosition}px`;
-  mario.style.animation = "none";
-  mario.style.bottom = `${marioPosition}px`;
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./images/mario-game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "45px";
+
+    clearInterval(loopGame);
+  }
 });
 
 document.addEventListener("keydown", jump);
